96e90c697f66d73d6f8b240b1516023e4baf82e8
PaulinaPiasta <paulina.piasta@finmancorp.com>
2021-10-25 12:30:34 +0200

    task 2 initial

---
+++ task2.py
@@ -0,0 +1,19 @@
+"""
+Test 2
+-----
+Scan all files in any folder
+e.g. usr/share/doc recursively and group them by first three letters.
+For each 3-letter group sort the filenames in that group by the
+actual size of the files in reverse order. Store the resulting structure in
+a dictionary called result
+
+Example:
+{
+'aam': [(42281, 'aamfigs.pdf'), (1219, 'aamfigs.tex')],
+'aas': [(113610, 'aassymbols.pdf'), (4753, 'aassymbols.tex.gz')],
+...
+'zxj': [(132386, 'zxjafont.pdf'), (3182, 'zxjafont.tex.gz'
+'zz_': [(654,'zz_translate_test.crm')]
+}
+"""
+

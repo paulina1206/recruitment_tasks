b30b591be3755e2a51d32661e9c616770736770a
PaulinaPiasta <paulina.piasta@finmancorp.com>
2021-10-25 12:00:33 +0200

    task 1 initial

---
+++ task1.py
@@ -0,0 +1,12 @@
+"""
+Test 1
+------
+Given the 2 lists a and b, produce a distinct list c that contains only the
+words that are shared by the 2 lists and let the list be sorted by number of
+characters in the words. Shortest words first in the list.
+"""
+
+a = ['century', 'customer', 'democratic', 'Congress', 'customer', 'evening',
+     'often', 'outside', 'reveal', 'weight', 'western', 'century']
+b = ['weapon', 'western', 'traditional', 'guess', 'customer', 'exist',
+     'democratic', 'Congress', 'evening', 'finish', 'western', 'executive']
